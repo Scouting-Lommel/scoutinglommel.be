cc671b752ae25bd33560e4f5006f44e4f2505fb7
Seppe Alaerts <hello@seppealaerts.be>
2026-08-22 14:56:14 +0200

    feat: link leaders section to who-is-wie detail modal

--- src/components/organisms/Leaders/index.tsx
+++ src/components/organisms/Leaders/index.tsx
@@ -1,20 +1,36 @@
+import Link from 'next/link';
 import type { JSX } from 'react';
 import Leader from '@/components/molecules/Leader';
 import { Leaders as LeadersProps } from './types';
 import './Leaders.css';
+
+const slugifyLeader = (firstName: string, lastName: string): string =>
+  `${firstName}-${lastName}`
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '');
 
 const Leaders = ({ leaders }: LeadersProps): JSX.Element => {
   return (
     <div className="leaders__container">
       <div className="leaders">
         {leaders.map((leader, i) => {
+          const href = `/wie-is-wie?leider=${slugifyLeader(leader.firstName, leader.lastName)}`;
+
           return (
-            <Leader
-              firstName={leader.firstName}
-              lastName={leader.lastName}
-              image={leader.image}
+            <Link
               key={i}
-            />
+              href={href}
+              className="leaders__leader-link"
+            >
+              <Leader
+                firstName={leader.firstName}
+                lastName={leader.lastName}
+                image={leader.image}
+              />
+            </Link>
           );
         })}
       </div>
